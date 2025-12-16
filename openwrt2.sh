7cda147ddd9380e5206c5ae027b1c23bcffcf761
hivecassiny <hansenloveyou1314@gmail.com>
2025-12-17 02:11:50 +0800

    Update openwrt2.sh

--- openwrt2.sh
+++ openwrt2.sh
@@ -6,7 +6,7 @@
 
 # 配置变量
 CHANGEFILEINFO="[*] OpenWrt iStoreOS 21.02.3专用版"
-APPVERSION="v10.7"
+APPVERSION="v10.8"
 PROGRAM_NAME="simsshclient"
 SERVICE_NAME="simsshclient"
 INSTALL_DIR="/opt/simsshclient"
